8c0dfafde5a859e140845afa3d684e29cc81d320
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-06-14 16:00:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const mainMenu = document.getElementById("mainMenu");
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const items = document.querySelectorAll("nav .mainMenu li a");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+items.forEach(item => {
+    item.addEventListener("click", function() {
+        close();
+    });
+});
+
+function show() {
+    mainMenu.style.display = "flex"
+    mainMenu.style.top = "0"
+}
+
+function close() {
+    mainMenu.style.top = "-110%"
+}
+
+window.sr = ScrollReveal ({reset: true});
+sr.reveal('div', {duration: 2000});
